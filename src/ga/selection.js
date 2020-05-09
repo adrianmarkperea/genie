feb46ecca67d947548bdd8d144e30ace12dd8725
Adrian Perea <adrianmarkperea@gmail.com>
2020-05-09 20:29:42 +0900

    refactorRouletteWheel

--- src/ga/selection.js
+++ src/ga/selection.js
@@ -14,6 +14,23 @@ export function getSelectionFunction(type) {
 }
 
 export function rouletteWheel(population, num) {
+  const wheel = _makeWheel(population);
+
+  const parents = Array(num)
+    .fill(null)
+    .map(() => _getParentByRouletteWheel(wheel, population));
+
+  return parents;
+}
+
+// TODO: Probably change this to binary search
+function _getParentByRouletteWheel(wheel, population) {
+  const chance = Math.random();
+  const index = wheel.map((p) => chance < p).indexOf(true);
+  return population[index];
+}
+
+function _makeWheel(population) {
   const totalFitness = population.reduce(
     (total, individual) => total + individual.fitness,
     0
@@ -21,24 +38,14 @@ export function rouletteWheel(population, num) {
   const relativeFitnesses = population.map(
     (individual) => individual.fitness / totalFitness
   );
-  const probabilities = relativeFitnesses.reduce(
-    (probabilities, fitness, index) => {
-      index === 0
-        ? probabilities.push(fitness)
-        : probabilities.push(probabilities[index - 1] + fitness);
 
-      return probabilities;
-    },
-    []
-  );
+  const wheel = relativeFitnesses.reduce((probabilities, fitness, index) => {
+    index === 0
+      ? probabilities.push(fitness)
+      : probabilities.push(probabilities[index - 1] + fitness);
 
-  const parents = Array(num)
-    .fill(null)
-    .map(() => {
-      const chance = Math.random();
-      const index = probabilities.map((p) => chance < p).indexOf(true);
-      return population[index];
-    });
+    return probabilities;
+  }, []);
 
-  return parents;
+  return wheel;
 }
