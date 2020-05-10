d74b2887e9c9964fe6fd69ac4422865108b79419
Adrian Perea <adrianmarkperea@gmail.com>
2020-05-10 17:27:53 +0900

    Aggregate modules

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as chromosomes from './chromosomes';
+import * as ga from './ga';
+import * as individual from './individual';
+import * as simulation from './simulation';
+
+export { chromosomes, ga, individual, simulation };
